8606437b704058685aa3f1f18cd18abbe2181a10
Aazib Abdullah <600021738@fjuhsd.org>
2021-02-11 05:02:42 +0000

    GetListofFactors: Created an ArrayList, and returned all the factors in an arraylist.
    keepOnlyCompositeNumbers:
    Held the values in 1 variable and removed the value if the factors of the arrayList is basically empty.

--- ArrayListFunHouse.java
+++ ArrayListFunHouse.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Aazib Abdullah
 
 import java.util.Scanner;
 import java.util.List;
@@ -12,9 +12,38 @@ public class ArrayListFunHouse
 {
  public static ArrayList<Integer> getListOfFactors(int number)
  {
+   // Declare an ArrayList
+   ArrayList<Integer> factors = new ArrayList<Integer>();
+  // Activate a for loop, set i to 2, and go up to the number before number, and add i.
+  for (int i = 2; i < number; i++)
+  {
+    // If the remainder of i is 0, add that value to i.
+    if (number % i == 0)
+    {
+      // add the value to i
+      factors.add(i);
+    }
+  }
+  // Return the ArrayList.
+  return factors;
  }
  
- public static void keepOnlyCompositeNumbers( List<Integer> nums )
+ public static void keepOnlyCompositeNumbers(List<Integer> nums)
  {
- }
+   // Declare a variable and set the value to 0
+   int n = 0;
+   // Activate the for loop traversing nums from size -1 to 0.
+   for (int i = nums.size()-1; i >= 0; i--)
+   {
+     // Initialize nums to nums.get(i).
+     n = nums.get(i);
+     // Create a new ArrayList called compositeFactors and set it to getListOfFactors with n inside of it
+     ArrayList<Integer> compositeFactors = getListOfFactors(n);
+     // If the ArrayList is empty, remove the i from the value.
+     if (compositeFactors.size() == 0)
+     {
+       nums.remove(i);
+     }
+    }
+  }
 }
